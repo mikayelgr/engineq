82a73866345cfc02d6a703f450e77bf8ce04ba67
Mikayel Grigoryan <michael.grigoryan25@gmail.com>
2025-04-02 22:15:55 +0400

    improve: refactor BraveSearchService to use instance client and enhance error handling

--- acura/internal/services/brave_search.py
+++ acura/internal/services/brave_search.py
@@ -1,36 +1,29 @@
 import httpx
 from dataclasses import dataclass
-import logging
 from internal.conf import Config
-
-logger = logging.getLogger(__name__)
-client = httpx.AsyncClient(base_url="https://api.search.brave.com", follow_redirects=True, headers={
-    "Accept": "application/json",
-})
 
 
 @dataclass
 class BraveSearchService:
-    async def search_youtube(query: str, num_results: int = 2) -> dict | None:
-        try:
-            query = "site:youtube.com" + ' ' + query
-            search_response = await client.get("/res/v1/web/search", params={
-                "q": query,
-                "country": "US",  # for now this will remain as the default
-                "ui-lang": "en-US",
-                "count": num_results,
-                "safesearch": "strict",
-                "text_decorations": False,
-            }, headers={"X-Subscription-Token": Config().BRAVE_SEARCH_TOKEN})
+    client = httpx.AsyncClient(
+        base_url="https://api.search.brave.com",
+        follow_redirects=True,
+        headers={"Accept": "application/json"})
 
-            if search_response.status_code in (302, 200):
-                search_response_dict = search_response.json()
-                if ("web" not in search_response_dict) or (len(search_response_dict["web"]["results"]) == 0):
-                    return None
+    async def search_youtube(self, query: str, num_results: int = 2):
+        query = "site:youtube.com" + ' ' + query
+        search_response = await self.client.get("/res/v1/web/search", params={
+            "q": query,
+            "country": "US",  # for now this will remain as the default
+            "ui-lang": "en-US",
+            "count": num_results,
+            "safesearch": "strict",
+            "text_decorations": False,
+        }, headers={"X-Subscription-Token": Config().BRAVE_SEARCH_TOKEN})
 
-                return search_response_dict["web"]["results"]
-
+        search_response.raise_for_status()
+        search_response_dict = search_response.json()
+        if ("web" not in search_response_dict) or (len(search_response_dict["web"]["results"]) == 0):
             return None
-        except Exception as e:
-            logger.error(e)
-            return None
+
+        return search_response_dict["web"]["results"]
