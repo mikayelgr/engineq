46120c6e4f9f97d3618f5fb0a086545587a53688
Mikayel Grigoryan <michael.grigoryan25@gmail.com>
2025-04-03 02:10:06 +0400

    improve: enhance BraveSearchService with retry logic and improved request handling

--- acura/internal/services/brave_search.py
+++ acura/internal/services/brave_search.py
@@ -1,30 +1,59 @@
 import httpx
+import asyncio
 from dataclasses import dataclass
 from internal.conf import Config
 
 
 @dataclass
-class BraveSearchService():
-    client = httpx.AsyncClient(
+class BraveSearchService:
+    __client = httpx.AsyncClient(
         base_url="https://api.search.brave.com",
         follow_redirects=True,
         headers={"Accept": "application/json"})
 
     @classmethod
-    async def search_youtube(self, query: str, num_results: int = 2):
+    async def _make_request(cls, endpoint: str, params: dict, headers: dict = None, max_retries: int = 5):
+        retries = 0
+        backoff = 1  # Initial backoff in seconds
+
+        while retries < max_retries:
+            try:
+                response = await cls.__client.get(endpoint, params=params, headers=headers)
+
+                if response.status_code == 429:
+                    retries += 1
+                    await asyncio.sleep(backoff)
+                    backoff *= 2  # Exponential backoff
+                    continue
+
+                response.raise_for_status()
+                return response.json()
+
+            except httpx.RequestError as e:
+                retries += 1
+                await asyncio.sleep(backoff)
+                backoff *= 2  # Exponential backoff
+                if retries >= max_retries:
+                    raise e
+
+        raise Exception(
+            "Max retries exceeded while trying to perform the request.")
+
+    @classmethod
+    async def search_youtube(cls, query: str, num_results: int = 2):
         query = "site:youtube.com" + ' ' + query
-        search_response = await self.client.get("/res/v1/web/search", params={
+        params = {
             "q": query,
             "country": "US",  # for now this will remain as the default
             "ui-lang": "en-US",
             "count": num_results,
             "safesearch": "strict",
             "text_decorations": False,
-        }, headers={"X-Subscription-Token": Config().BRAVE_SEARCH_TOKEN})
+        }
+        headers = {"X-Subscription-Token": Config().BRAVE_SEARCH_TOKEN}
 
-        search_response.raise_for_status()
-        search_response_dict = search_response.json()
-        if ("web" not in search_response_dict) or (len(search_response_dict["web"]["results"]) == 0):
+        response_data = await cls._make_request("/res/v1/web/search", params=params, headers=headers)
+        if ("web" not in response_data) or (len(response_data["web"]["results"]) == 0):
             return None
 
-        return search_response_dict["web"]["results"]
+        return response_data["web"]["results"]
