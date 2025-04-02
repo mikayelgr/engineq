dbcdd6552fd1071be2391b21eeedd65836cd149d
Mikayel Grigoryan <michael.grigoryan25@gmail.com>
2025-04-03 02:10:57 +0400

    improve: quality of the songs generated has been improved due to the levenshtein+LLM agentic workflow search from spotify+youtube

--- acura/internal/chain.py
+++ acura/internal/chain.py
@@ -1,29 +1,18 @@
-from spotipy import SpotifyClientCredentials
 from pydantic_ai import Agent
 from internal.models import Prompts
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncConnection
-from pydantic import BaseModel, Field
-from spotipy import Spotify as Spotipy
-from internal.conf import Config
 import logging
-import random
 from dataclasses import dataclass
-import string
+from internal.services.spotify import SpotifyService
 from internal.services.brave_search import BraveSearchService
 from internal.agents import decide_llm
 from internal.agents.yt_analyzer_agent import YoutubeAnalyzerAgent
 from pydantic_graph import BaseNode, GraphRunContext, End, Graph
+import Levenshtein
 
 # Maximum number of retries for executing the workflow
 MAX_RETRIES = 3
-
-spotipy = Spotipy(
-    auth_manager=SpotifyClientCredentials(
-        client_id=Config().SPOTIFY_CLIENT_ID,
-        client_secret=Config().SPOTIFY_CLIENT_SECRET
-    )
-)
 
 
 @dataclass
@@ -42,22 +31,15 @@ class GraphDeps:
 
 @dataclass
 class GenerateSearchQueryNode(BaseNode[GraphState, GraphDeps]):
-    @dataclass
-    class AgentOutput(BaseModel):
-        query: str | None = Field(
-            default=None,
-            description="The search query to find tracks on Spotify.")
-        exclude_explicit_tracks: bool = Field(
-            default=True,
-            description="Whether to exclude explicit tracks from the search results."
-        )
-
+    """
+    Generates a concise search query based on the user's prompt.
+    """
     logger = logging.getLogger(__name__)
     query_gen_agent = Agent(
         model=decide_llm(),
         retries=5,
         result_retries=3,
-        result_type=AgentOutput,
+        result_type=str,
         model_settings={"temperature": 0.7},
         # The system prompts follow CodeSignal's MPF format for GenAI prompts.
         # Learn more here: https://codesignal.com/learn/paths/prompt-engineering-for-everyone?courseSlug=understanding-llms-and-basic-prompting-techniques&unitSlug=mastering-consistent-formatting-and-organization-for-effective-prompting
@@ -79,163 +61,174 @@ __CONSTRAINTS__
   - Targets unique musical aspects to ensure diverse search results and minimize duplication issues in the database.
 - Maintains a professional tone aligned with business needs.
 - Avoids extraneous or unrelated details.
-
-__EXAMPLE__
-*User Prompt:* "I own a modern coffee shop and want upbeat acoustic music to create a lively yet cozy atmosphere."
-
-*Generated Search Queries:*
-1. "Upbeat acoustic coffeehouse"
-2. "Lively unplugged café tunes"
-3. "Energetic acoustic ambiance"
 """
     )
 
-    async def run(self, ctx: GraphRunContext[GraphState, GraphDeps]) -> 'SearchSpotifyNode' | End[None]:
-        if ctx.state.retry_count > MAX_RETRIES:
-            self.logger.error("Max retries exceeded for query generation.")
-            return End(None)
-
+    async def run(self, ctx: GraphRunContext[GraphState, GraphDeps]) -> 'RetrieveSpotifyPlaylistsNode':
         try:
             ambiance_prompt = (await ctx.deps.pg.execute(select(Prompts).where(Prompts.sid == ctx.deps.sid))).one()
-            query_flow = await self.query_gen_agent.run(ambiance_prompt.prompt)
-            # random character for truly random search query. for more information check the following
-            # stackoverflow thread: https://stackoverflow.com/questions/68006378/spotify-api-randomness-in-search
-            seed = random.choice(string.ascii_letters)
-            query = query_flow.data.query + ' ' + seed
-            return SearchSpotifyNode(query, query_flow.data.exclude_explicit_tracks)
+            flow = await self.query_gen_agent.run(ambiance_prompt.prompt)
+            return RetrieveSpotifyPlaylistsNode(query=flow.data)
         except Exception as e:
             self.logger.error(f"Error during query generation: {e}")
-            ctx.state.retry_count += 1
-            return GenerateSearchQueryNode()
+            raise
+
+
+@dataclass
+class RetrieveSpotifyPlaylistsNode(BaseNode[GraphState, GraphDeps]):
+    """
+    Retrieves Spotify playlists.
+    """
+    query: str
+    next_page_url: str | None = None
+
+    async def run(self, _: GraphRunContext[GraphState, GraphDeps]) -> 'MatchSpotifyPlaylistNode':
+        try:
+            playlists = await SpotifyService.get_spotify_curated_playlists(next=self.next_page_url)
+            if len(playlists.get("items", [])) == 0:
+                return End(None)
+
+            return MatchSpotifyPlaylistNode(query=self.query, playlists=playlists)
+        except Exception as e:
+            logging.getLogger(__name__).error(
+                f"Error retrieving Spotify playlists: {e}")
+            return End(None)
+
+
+@dataclass
+class MatchSpotifyPlaylistNode(BaseNode[GraphState, GraphDeps]):
+    """
+    Matches Spotify playlists against the generated query and filters tracks using an agent.
+    """
+    query: str
+    playlists: dict
+
+    playlist_filter_agent = Agent(
+        model=decide_llm(),
+        retries=3,
+        result_retries=2,
+        result_type=bool,
+        model_settings={"temperature": 0.5},
+        system_prompt="""
+__CONTEXT__
+You are an expert in understanding and interpreting playlist titles and descriptions
+and inferring whether they might contain the music that a user might want from a
+certain query.
+
+__ASK__
+Determine if a given playlist matches user's query or might include any music
+which is related to user's request. The user will give you the query, as well
+as the title and the description of the playlist.
+"""
+    )
+
+    async def run(self, _: GraphRunContext[GraphState, GraphDeps]) -> 'SearchYoutubeNode' | End[None]:
+        filtered_tracks = []
+        selected_playlist = None
+
+        for playlist in self.playlists.get("items", []):
+            try:
+                flow = await self.playlist_filter_agent.run(f"Desired Tracks in Playlist: {self.query}\nPlaylist title: {playlist['name']}\nPlaylist description: {playlist['description']}")
+                is_playlist_match = flow.data
+                if is_playlist_match:
+                    selected_playlist = playlist
+                    break
+
+            except Exception as e:
+                logging.getLogger(__name__).error(
+                    f"Error filtering playlist '{playlist['name']}': {e}")
+
+        if selected_playlist is not None:
+            entries = await SpotifyService.get_playlist_entries_by_id(selected_playlist["id"])
+            for entry in entries:
+                track = entry.get("track")
+                if not track.get("explicit", False):
+                    filtered_tracks.append(track)
+        else:
+            logging.getLogger(__name__).warning(
+                f"No matching playlists found for: '{self.query}'. Trying next page...")
+            return RetrieveSpotifyPlaylistsNode(query=self.query, next_page_url=self.playlists.get("next"))
+
+        if filtered_tracks:
+            return SearchYoutubeNode(tracks=filtered_tracks)
+
+        return End(None)
+
+
+@dataclass
+class SearchYoutubeNode(BaseNode[GraphState, GraphDeps]):
+    """
+    Searches YouTube for videos of the corresponding tracks.
+    """
+    tracks: list[dict]
+
+    async def run(self, _: GraphRunContext[GraphState, GraphDeps]) -> 'VerifyYoutubeNode':
+        tracks = []
+        for track in self.tracks:
+            query = f"{track['name']} {track['artists'][0]['name']}"
+            try:
+                results = await BraveSearchService.search_youtube(query, 2)
+                # This data will be later used to verify if the video is a music video
+                # when searching for the track on YouTube.
+                tracks.append(
+                    {"spotify_track": track, "youtube_results": results})
+            except Exception as e:
+                logging.getLogger(__name__).error(
+                    f"Error searching YouTube for track '{track['name']}': {e}")
+        return VerifyYoutubeNode(tracks)
 
 
 @dataclass
 class VerifyYoutubeNode(BaseNode[GraphState, GraphDeps]):
     """
-    This node makes sure that the tracks from Spotify actually exist on YouTube by searching
-    for them using Brave's search API. Tracks must be received from `search_spotify` tool in
-    the workflow.
+    Verifies YouTube results to ensure they are music videos.
     """
     tracks: list[dict]
-    logger = logging.getLogger(__name__)
 
-    async def run(self, ctx: GraphRunContext[GraphState, GraphDeps]) -> GenerateSearchQueryNode | End[list[dict]]:
-        for ti in range(len(self.tracks) - 1, -1, -1):
-            t = self.tracks[ti]
-            query = f"{t['name']} {t['artists'][0]['name']}"
+    async def run(self, _: GraphRunContext[GraphState, GraphDeps]) -> End[list[dict]]:
+        verified_tracks = []
+        for i in range(len(self.tracks)):
+            spotify_track = self.tracks[i]["spotify_track"]
+            youtube_results = self.tracks[i]["youtube_results"]
+            for youtube_result in youtube_results:
+                similarity = Levenshtein.ratio(
+                    f"{spotify_track['name'].lower()} - {spotify_track['artists'][0]['name'].lower()}",
+                    youtube_result["title"].lower()
+                )
 
-            try:
-                # It is virtually impossible to not get any results from YouTube from Brave
-                # based on what we have seen so far.
-                results = await BraveSearchService.search_youtube(query, 2)
-            except Exception as e:
-                self.logger.error(f"Error during Youtube verification: {e}")
-                continue
+                # Here, we are using Levenshtein distance to check if the title of the
+                # YouTube video is similar to the track name. If the similarity is above
+                # a certain threshold (0.65), we consider it a match.
+                #
+                # This way, we don't need to trigger another LLM agent to check if the
+                # video is a music video. We can assume that if the title is similar to
+                # the track name, it is likely a music video.
+                if youtube_result and similarity > 0.65:
+                    verified_tracks.append({
+                        "title": spotify_track["name"],
+                        "uri": youtube_result["url"],
+                        "artist": spotify_track["artists"][0]["name"],
+                        "duration": spotify_track["duration_ms"] / 1000,
+                        "explicit": spotify_track["explicit"],
+                        "image": spotify_track.get("album", {}).get("images", [{}])[0].get("url", None),
+                    })
 
-            if len(results) == 0:
-                self.logger.warning(f"No results found for '{query}'")
-                # Removing the track at the index
-                self.tracks.pop(ti)
-                continue
+                    break
 
-            # Trigger the YoutubeAnalyzerAgent to check if the video is a music video
-            # or contains music solely based on the title.
-            if await YoutubeAnalyzerAgent().check_is_music_video(results[0]["title"]):
-                self.tracks[ti] = {
-                    "title": t["name"],
-                    "uri": results[0]["url"],
-                    "artist": t["artists"][0]["name"],
-                    "duration": t["duration_ms"] / 1000,
-                    "explicit": t.get("explicit", False),
-                    "image": results[0].get("thumbnail", None).get("src", None),
-                }
-
-                continue
-
-            # Removing the track at the index in case it is not a music video
-            self.tracks.pop(ti)
-
-        # If no tracks are left, we need to generate a new search query
-        # and start the process again.
-        if len(self.tracks) == 0:
-            ctx.state.retry_count += 1
-            return GenerateSearchQueryNode()
-
-        return End(self.tracks)
-
-
-@dataclass
-class SearchSpotifyNode(BaseNode[GraphState, GraphDeps]):
-    query: str  # Search query as generated by the GenerateSearchQueryNode.
-    exclude_explicit_tracks: bool
-    logger = logging.getLogger(__name__)
-
-    async def run(self, _: GraphRunContext[GraphState, GraphDeps]) -> VerifyYoutubeNode | GenerateSearchQueryNode:
-        try:
-            # TODO: Analyze the titles and descriptions of the playlists and filter out the
-            # most suitable ones for the given prompt of the user. This can be done using a
-            # separate AI agent, which will take in the playlists and the prompt and
-            # filter them out based on the prompt.
-            #
-            # Potential implementation start:
-            # # search only for tracks for tracks on Spotify. uniqueness is almost guaranteed at this point
-            # spotify_curated_playlists = spotipy.user_playlists("spotify")
-            # if spotify_curated_playlists is None:
-            #     self.logger.warning("Spotify API mishandled the request.")
-            #     return End(None)
-
-            spotify_search_response = spotipy.search(self.query, type="track")
-            tracks = spotify_search_response["tracks"]["items"]
-            if self.exclude_explicit_tracks:
-                tracks = [t for t in tracks if not t["explicit"]]
-
-            return VerifyYoutubeNode(tracks)
-        except Exception as e:
-            self.logger.error(f"Error during Spotify search: {e}")
-            return GenerateSearchQueryNode()
+        return End(verified_tracks)
 
 
 @dataclass
 class MusicDiscoveryPipeline:
     """
-    MusicDiscoveryPipeline is responsible for orchestrating a series of nodes
-    to facilitate the discovery of music. It utilizes a directed graph structure
-    to define the flow of operations, where each node performs a specific task
-    in the pipeline.
-
-    Attributes:
-        graph (Graph): A directed graph that defines the sequence of operations
-            in the pipeline. The graph consists of the following nodes:
-            - GenerateSearchQueryNode: Responsible for generating search queries
-              based on input data.
-            - SearchSpotifyNode: Executes the search query on Spotify to retrieve
-              potential music matches.
-            - VerifyYoutubeNode: Verifies the retrieved music matches by cross-referencing
-              them with YouTube data.
-            The graph is configured to produce a list of dictionaries as its final output.
-
-    Methods:
-        run(deps: GraphDeps) -> list[dict]:
-            Executes the pipeline by running the graph with the specified dependencies.
-            The pipeline starts with the `GenerateSearchQueryNode` and progresses through
-            the defined nodes in the graph. If the graph execution does not produce any
-            output, a `ValueError` is raised.
-
-            Args:
-                deps (GraphDeps): Dependencies required for the execution of the graph.
-                    These may include external services, configurations, or other resources.
-
-            Returns:
-                list[dict]: The final output of the pipeline, which is a list of dictionaries
-                containing the discovered music data.
-
-            Raises:
-                ValueError: If the graph execution does not produce any output.
+    Orchestrates the music discovery process.
     """
-
-    graph = Graph(nodes=(GenerateSearchQueryNode, SearchSpotifyNode, VerifyYoutubeNode),
-                  run_end_type=list[dict],
-                  name="Music Discovery Pipeline")
+    graph = Graph(
+        nodes=(GenerateSearchQueryNode, RetrieveSpotifyPlaylistsNode,
+               MatchSpotifyPlaylistNode, SearchYoutubeNode, VerifyYoutubeNode),
+        run_end_type=list[dict],
+        name="Music Discovery Pipeline"
+    )
 
     async def run(self, deps: GraphDeps):
         flow = await self.graph.run(
